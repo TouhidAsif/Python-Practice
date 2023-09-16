7677818bdcf38c7f0608e9e5be29368974ec6f38
SQUAREHOSPITAL\touhid <touhid@squarehospital.com>
2023-09-16 11:03:11 +0600

    GUI application with database connection in python

--- PythonGuiDb.py
+++ PythonGuiDb.py
@@ -5,4 +5,13 @@ myroot.maxsize(600,600)
 myroot.minsize(600,600)
 myroot.title("Employee Registration Form")
 
+
+lb1=Label(text="Enter Employee Id")
+lb1.grid(row=0,column=0)
+userid=IntVar()
+txtid=Entry(textvariable=userid)
+txtid.grid(row=0,column=2)
+
+
+
 myroot.mainloop()
