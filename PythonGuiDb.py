5d449a2e849d1167ad1faf4f67f756eaccad0008
SQUAREHOSPITAL\touhid <touhid@squarehospital.com>
2023-09-16 11:44:00 +0600

    GUI application with database connection in python

--- PythonGuiDb.py
+++ PythonGuiDb.py
@@ -1,17 +1,33 @@
 from tkinter import *
-myroot=Tk()
+
+myroot = Tk()
 myroot.geometry('600x600')
-myroot.maxsize(600,600)
-myroot.minsize(600,600)
+myroot.maxsize(600, 600)
+myroot.minsize(600, 600)
 myroot.title("Employee Registration Form")
 
+lb1 = Label(text="Enter Employee Id", font=('Arial,10,bold'), fg="red")
+lb1.grid(row=0, column=0)
+eid = IntVar()
+txtid = Entry(textvariable=eid, font=('Arial,10,bold'), fg="blue")
+txtid.grid(row=0, column=1)
 
-lb1=Label(text="Enter Employee Id")
-lb1.grid(row=0,column=0)
-userid=IntVar()
-txtid=Entry(textvariable=userid)
-txtid.grid(row=0,column=2)
+lb2 = Label(text="Enter Employee Name", font=('Arial,10,bold'), fg="red")
+lb2.grid(row=1, column=0)
+ename = StringVar()
+txtename = Entry(textvariable=ename, font=('Arial,10,bold'), fg="blue")
+txtename.grid(row=1, column=1)
 
+lb3 = Label(text="Enter Employee Department", font=('Arial,10,bold'), fg="red")
+lb3.grid(row=2, column=0)
+edept = StringVar()
+txtdept = Entry(textvariable=edept, font=('Arial,10,bold'), fg="blue")
+txtdept.grid(row=2, column=1)
 
+lb4 = Label(text="Enter Employee Salary", font=('Arial,10,bold'), fg="red")
+lb4.grid(row=3, column=0)
+esalary = StringVar()
+txtsalary = Entry(textvariable=esalary, font=('Arial,10,bold'), fg="blue")
+txtsalary.grid(row=3, column=1)
 
 myroot.mainloop()
