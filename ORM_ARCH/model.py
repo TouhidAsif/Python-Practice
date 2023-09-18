77f63244987938249c72b3982ca3e90ee85cc491
SQUAREHOSPITAL\touhid <touhid@squarehospital.com>
2023-09-18 09:50:59 +0600

    orm architecture in python with mysql db

---
+++ ORM_ARCH/model.py
@@ -0,0 +1,24 @@
+class Employee:
+    def setid(self, id):
+        self.id = id
+
+    def getid(self):
+        return self.id
+
+    def setname(self, name):
+        self.name = name
+
+    def getname(self):
+        return self.name
+
+    def setdept(self, dept):
+        self.dept = dept
+
+    def getdept(self):
+        return self.dept
+
+    def setsalary(self, salary):
+        self.salary = salary
+
+    def getsalary(self):
+        return self.salary
