5f954fb066735ec7560b41a749d242465889a5a4
SQUAREHOSPITAL\touhid <touhid@squarehospital.com>
2023-09-18 16:20:24 +0600

    search by id

--- ORM_ARCH/employeedao.py
+++ ORM_ARCH/employeedao.py
@@ -1,15 +1,32 @@
 import myconnection as mcon
-import  model
+import model
+
+
 class EmployeeDao:
 
     def __init__(self):
-        self.con=mcon.MyConnection.getConnection()
-        self.cur=self.con.cursor()
-    def insertEmployee(self,E):
+        self.con = mcon.MyConnection.getConnection()
+        self.cur = self.con.cursor()
+
+    def insertEmployee(self, E):
         try:
-            sql="insert into employee values('%d','%s','%s','%d')"
-            value=(E.getid(),E.getname(),E.getdept(),E.getsalary())
+            sql = "insert into employee values('%d','%s','%s','%d')"
+            value = (E.getid(), E.getname(), E.getdept(), E.getsalary())
             self.cur.execute(sql % value)
             self.con.commit()
         except Exception as obj:
             print(obj)
+
+    def searchEmployee(self, empid):
+        try:
+            sql = "select * from employee where id=%d"
+            self.cur.execute(sql % empid)
+            result = self.cur.fetchone()
+            E1 = model.Employee()
+            E1.setid(result[0])
+            E1.setname(result[1])
+            E1.setdept(result[2])
+            E1.setsalary(result[3])
+            return E1
+        except Exception as msg:
+            print(msg)
--- ORM_ARCH/main.py
+++ ORM_ARCH/main.py
@@ -4,18 +4,31 @@ import employeedao as empdao
 
 con1 = ms.MyConnection.getConnection()
 print("connection success!")
-E1 = model.Employee()
-eid=int(input("Enter employee id: "))
-ename=input("Enter your name: ")
-edept=input("Enter department: ")
-esalary=int(input("Enter your salary: "))
+E1=model.Employee()
+# for search one by id
+eid=int(input("Enter employee id for search: "))
+ed=empdao.EmployeeDao()
+E=ed.searchEmployee(eid)
 
-
-
-E1.setid(eid)
-E1.setname(ename)
-E1.setdept(edept)
-E1.setsalary(esalary)
-ed = empdao.EmployeeDao()
-ed.insertEmployee(E1)
-print("record inserted............")
+if E is not None:
+    print("emp id :", E.getid())
+    print("emp name :", E.getname())
+    print("emp dept :", E.getdept())
+    print("emp salary :", E.getsalary())
+else:
+    print(f"No employee found with ID {eid}")
+# for insert......
+# eid=int(input("Enter employee id: "))
+# ename=input("Enter your name: ")
+# edept=input("Enter department: ")
+# esalary=int(input("Enter your salary: "))
+#
+#
+#
+# E1.setid(eid)
+# E1.setname(ename)
+# E1.setdept(edept)
+# E1.setsalary(esalary)
+# ed = empdao.EmployeeDao()
+# ed.insertEmployee(E1)
+# print("record inserted............")
