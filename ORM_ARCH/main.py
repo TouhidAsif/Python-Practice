1a573a4c3352f7de2a416fa1621b9843bb1b6a1d
SQUAREHOSPITAL\touhid <touhid@squarehospital.com>
2023-09-18 15:59:09 +0600

    inserted record from user entry

--- ORM_ARCH/main.py
+++ ORM_ARCH/main.py
@@ -5,10 +5,17 @@ import employeedao as empdao
 con1 = ms.MyConnection.getConnection()
 print("connection success!")
 E1 = model.Employee()
-E1.setid(201)
-E1.setname("Asif")
-E1.setdept("ICT")
-E1.setsalary(100000)
+eid=int(input("Enter employee id: "))
+ename=input("Enter your name: ")
+edept=input("Enter department: ")
+esalary=int(input("Enter your salary: "))
+
+
+
+E1.setid(eid)
+E1.setname(ename)
+E1.setdept(edept)
+E1.setsalary(esalary)
 ed = empdao.EmployeeDao()
 ed.insertEmployee(E1)
 print("record inserted............")
